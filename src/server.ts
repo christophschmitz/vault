d5bf1e80743fbef829508fd78208f4804a9dc4eb
christophschmitz <c.schmitz@outlook.de>
2021-08-11 12:05:45 +0200

    🚚  Add routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import express from 'express';
-import { readCredentials } from './utils/credentials';
+import { getCredential, readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
 
-app.get('/api/credentials/:search', async (_request, response) => {
-  response.status(200).json(readCredentials);
+app.get('/api/credentials/', async (_request, response) => {
+  try {
+    const credentials = await readCredentials();
+    response.status(200).json(credentials);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Internal Server Error! Please try again later.');
+  }
+});
+
+app.get('/api/credentials/:service', async (request, response) => {
+  const { service } = request.params;
+  try {
+    const credential = await getCredential(service);
+    response.status(200).json(credential);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send(`Could not find services ${service}`);
+  }
 });
 
 app.get('/', (_request, response) => {
