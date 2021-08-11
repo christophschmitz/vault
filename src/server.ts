f22a5670ab622092b2843f2f07c5c23fc7b63607
christophschmitz <c.schmitz@outlook.de>
2021-08-11 08:56:24 +0200

    ✨  Import read credentails

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
+
+app.get('/api/credentials/:search', async (_request, response) => {
+  response.status(200).json(readCredentials);
+});
 
 app.get('/', (_request, response) => {
   response.send('Hello World!');
