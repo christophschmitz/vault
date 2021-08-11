e4b4dbef967a5e598df4f69814a8b17ff2343c06
christophschmitz <c.schmitz@outlook.de>
2021-08-11 14:40:28 +0200

    ✨  Add addCredential function to DB

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,4 +1,6 @@
+import { read, writeFile } from 'fs/promises';
 import { readFile } from 'fs/promises';
+import { write } from 'node:fs';
 import { DB, Credential } from '../types';
 
 export async function readCredentials(): Promise<Credential[]> {
@@ -17,4 +19,17 @@ export async function getCredential(service: string): Promise<Credential> {
     throw new Error(`No credential found for service: ${service}`);
   }
   return credential;
+}
+
+export async function addCredential(credential: Credential): Promise<void> {
+  //read existing credentials
+  const credentials = await readCredentials();
+  // add argument(credential) to existing credentials
+  const newCredentials = [...credentials, credential];
+  //create a news
+  const newDB: DB = {
+    credentials: newCredentials,
+  };
+  // overwrite DB using writeFile :tada:
+  await writeFile('./src/db.json', JSON.stringify(newDB));
 }
