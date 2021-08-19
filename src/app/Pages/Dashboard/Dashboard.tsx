6eebe61f33c1e6b2ae12886b8e7684dc60d89cc3
christophschmitz <c.schmitz@outlook.de>
2021-08-19 10:01:27 +0200

    🎨  Add main element to dashboard

--- src/app/Pages/Dashboard/Dashboard.tsx
+++ src/app/Pages/Dashboard/Dashboard.tsx
@@ -3,10 +3,10 @@ import styles from './Dasboard.module.css';
 
 export default function Dashboard(): JSX.Element {
   return (
-    <>
+    <main className={styles.container}>
       <h1 className={styles.heading}>🗄 MYVAULT</h1>
       <p className={styles.para}>My personal password Manager</p>
       <input className={styles.inputfield}></input>
-    </>
+    </main>
   );
 }
