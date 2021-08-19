fd3bb46e8435d06eb8d850798e69a386af150af1
christophschmitz <c.schmitz@outlook.de>
2021-08-19 07:46:10 +0200

    ✏️  Change heading text

--- src/app/Pages/Dashboard/Dashboard.tsx
+++ src/app/Pages/Dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ import styles from './Dasboard.module.css';
 export default function Dashboard(): JSX.Element {
   return (
     <>
-      <h1 className={styles.heading}>Vault</h1>
+      <h1 className={styles.heading}>🗄 MYVAULT</h1>
       <p className={styles.para}>My personal password Manager</p>
       <input className={styles.inputfield}></input>
     </>
