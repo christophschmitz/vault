83a4038aaf06fa3852a49c4e0e0cb765a93141d6
christophschmitz <c.schmitz@outlook.de>
2021-08-20 17:05:32 +0200

    ✨ Add card component

--- src/app/Components/Cardcomponent/Cardcomponent.tsx
+++ src/app/Components/Cardcomponent/Cardcomponent.tsx
@@ -15,9 +15,7 @@ export default function CredentialCard({
   return (
     <div className={styles.card}>
       <i className={`fab fa-${credentialData.service} fa-5x`}></i>
-      <p className={styles.subheading}>Username:</p>
       <p>{credentialData.username}</p>
-      <p className={styles.subheading}>Password:</p>
       <p>{credentialData.password}</p>
       <button>EDIT</button>
       <button>DELETE</button>
