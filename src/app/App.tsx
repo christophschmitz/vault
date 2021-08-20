a6ef69f28323f8939100e68de50ebaa514a3af26
christophschmitz <c.schmitz@outlook.de>
2021-08-20 17:58:21 +0200

    🚚  Add routes to App tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import Password from './Pages/Password/Password';
+import CreateCredentials from './Pages/CreateCredentials/CreateCredentials';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/add">
+          <CreateCredentials />
+        </Route>
+        <Route path="/services"></Route>
         <Route path="/password/:service">
           <Password />
         </Route>
